9929e53789d99f859a42d0ebbbb738c6d18ff3a0
drackdude <arjuncm201@gmail.com>
2024-02-11 09:32:13 +0530

    added speak and listen

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 import speech_recognition as sp
 import pyttsx3 as tx
 
+listener = sp.Recognizer()
+speaker = tx.init()
+
+def listen():
+    with sp.Microphone() as microphone:
+        print("Listening...")
+        listener.adjust_for_ambient_noise(microphone)
+        audio = listener.listen(microphone)
+        try:
+            text = listener.recognize_google(audio)
+            print("text: ", text)
+            return text
+        except sp.UnknownValueError as e:
+            print("error: ", e,"error 1")
+        except sp.RequestError as e:
+            print("error: ", e,"error 2")
+        return ""
+
+def speak(text):
+    speaker.say(text)
+    speaker.runAndWait()
+
